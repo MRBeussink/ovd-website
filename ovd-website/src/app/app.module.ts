25124b179fd4a89b87d1ca67a8f2c7556596654f
MRBeussink <MRBeussink@gmail.com>
2019-03-17 15:58:14 -0500

    Componentized
    Refactored so that different sections were seperate components

--- ovd-website/src/app/app.module.ts
+++ ovd-website/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { DocsComponent } from './docs/docs.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      AboutComponent,
+      TeamComponent,
+      DocsComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
